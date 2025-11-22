91e52d1e07910a38fa5324f5ba6dbbb9a5fb85db
TUMELO <cse24-043@thuto.bac.ac.bw>
2025-11-22 12:52:13 +0200

    Delete LoginController.java

--- LoginController.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-public class LoginController {
-    // demo credentials: admin / password
-    public boolean authenticate(String username, String password){
-        return "admin".equals(username) && "password".equals(password);
-    }
-}
